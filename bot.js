dd2996917636d539fe94e78794780611b2d94080
dodowael66 <57775220+dodowael66@users.noreply.github.com>
2019-11-15 07:57:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,31 +1,48 @@
-const Discord = require('discord.js');
-const client = new Discord.Client();
-console.log("Dodo");
-var prefix = "=";
-client.on("message", message => {
+const Discord = require("discord.js");
+  const kboosh = new Discord.Client();
+    const kbooshtoken = "NjQ0NjU2NzEzMDMwNTY1OTQz.Xc48cA.awwdQgsX5pgeka0pkSoj5sdfxh0"
+      kboosh.on('ready', () => {
+        kboosh.user.setGame(`KBOOSH TEST Broadcast`,'https://www.twitch.tv/TEST-Broadcast');
+          console.log('Im Ready!');
+  
+        });
 
-            if (message.content.startsWith(prefix + "la")) {
-                         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
-  let args = message.content.split(" ").slice(1);
-  var argresult = args.join(' '); 
-  message.guild.members.filter(m => m.presence.status !== 'offline').forEach(m => {
- m.send(`${argresult}\n ${m}`);
-})
- message.channel.send(`\`${message.guild.members.filter(m => m.presence.status !== 'online').size}\` : عدد الاعضاء المستلمين`); 
- message.delete(); 
-};     
+  kboosh.on('message', message => {
+    if (message.content.split(' ')[0] == '%k')
+       message.guild.members.forEach( member => {
+         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+
+
+           member.send( `${member} ! ` + "**" + message.guild.name + " : ** " + message.content.substr(3));
+                                                      message.delete();
+            
+                                                    });
+            
+                                                  });
+   kboosh.on("message", message => {
+       var prefix = "%";
+ 
+             var args = message.content.substring(prefix.length).split(" ");
+                if (message.content.startsWith(prefix + "b")) {
+                          if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+
+                          if (!args[1]) {
+                            
+                                 let embed3 = new Discord.RichEmbed()
+                                     .setDescription(":white_check_mark: | تم ارسال رسالة لا يوجد فيها شيء")
+                                       .setColor("#FF00FF")
+                                          message.channel.sendEmbed(embed3);
+                            
+                                        } else {
+
+                            
+                                           let embed4 = new Discord.RichEmbed()
+                                                            .setDescription(':white_check_mark: | تم ارسال الرساله للجميع ..')
+                                                                .setColor("#99999")
+                               
+                                                                message.channel.sendEmbed(embed4);
+                                                      message.delete();
+                            }
+                          }
 });
-
-client.on('ready', () => {
-   console.log(`----------------`);
-      console.log(`Desert Bot- Script By : darkxx`);
-        console.log(`----------------`);
-      console.log(`ON ${client.guilds.size} Servers '     Script By : black ' `);
-    console.log(`----------------`);
-  console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`=bc |GAZAREN AG |server`,"http://twitch.tv/Death Shop")
-client.user.setStatus("dnd")
-});
-
-client.login(process.env.NjQ0NjU2NzEzMDMwNTY1OTQz.Xc48cA.awwdQgsX5pgeka0pkSoj5sdfxh0
-);
+kboosh.login(NjQ0NjU2NzEzMDMwNTY1OTQz.Xc48cA.awwdQgsX5pgeka0pkSoj5sdfxh0);
